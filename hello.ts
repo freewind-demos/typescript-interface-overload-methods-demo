3ea1576ea9faa666af6d6626a84b71c6bb534435
Freewind <nowindlee@gmail.com>
2020-04-22 19:13:23 +0800

    typescript-class-overload-methods-demo

--- hello.ts
+++ hello.ts
@@ -1,1 +1,15 @@
-console.log('Hello');
+class User {
+  hello(name: number): void;
+  hello(name: string): void;
+  hello(name: number | string) {
+    if (typeof name === 'string') {
+      console.log(`Hello, ${name} (string)`);
+      return;
+    }
+    console.log(`Hello, ${name} (number)`);
+  }
+}
+
+const user = new User();
+user.hello('aaa');
+user.hello(111);
